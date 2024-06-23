5ff8b34383a83278584d6bfef4136993c3b4633a
Pavel Mykhailov <11407792+pashamykhailov@users.noreply.github.com>
2024-06-23 23:30:47 +0200

    Time: 104 ms (87.05%) | Memory: 57.5 MB (50.12%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.ts
+++ 2-add-two-numbers/add-two-numbers.ts
@@ -4,7 +4,7 @@ function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | nul
     let carry = 0; 
     while(l1 !== null || l2 !== null) {
         let x = l1 ? l1.val : 0;
-        let y = l2 ? l2.val : 0;    
+        let y = l2 ? l2.val : 0;
         let sum = carry + x + y;
         carry = Math.floor(sum / 10);
         cur.next = new ListNode(sum % 10);
