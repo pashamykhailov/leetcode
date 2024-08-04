5925801ace4d86c4b37604726aa3a2fd996f3289
Pavel Mykhailov <11407792+pashamykhailov@users.noreply.github.com>
2024-08-04 20:17:48 +0200

    Time: 106 ms (43.61%) | Memory: 55.2 MB (75.63%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.ts
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.ts
@@ -1,25 +1,18 @@
 function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
     const firstArrLength = nums1.length;
     const secondArrLength = nums2.length;
-    // here we defining length of both arrays
-
-    // here we covering edge case if first array length more than second arr length
     if(firstArrLength > secondArrLength) {
         return findMedianSortedArrays(nums2, nums1);
     }
 
-    // total length of combined arr and half len
 
     const totalLength = firstArrLength + secondArrLength;
     const halfLength = Math.floor(totalLength / 2);
 
-    // bounrdries for binary search 
     let start = 0;
     let end = firstArrLength;
 
     while(start <= end) {
-        // we need this and especially second pivot calculate from first pivot do move 
-        // dynamicly between two arrays independently
         const pivotFistArr = Math.floor((start + end) / 2);
         const pivotSecondArr = halfLength - pivotFistArr;
 
@@ -28,11 +21,6 @@ function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
 
         const secondArrLeftItem = pivotSecondArr === 0 ? Number.NEGATIVE_INFINITY : nums2[pivotSecondArr - 1];
         const secondArrRightItem = pivotSecondArr === secondArrLength ? Number.POSITIVE_INFINITY : nums2[pivotSecondArr];
-
-        // conditions to define if we match needed elems with binary
-        // we need compare values like cross to cross to check 
-        // if all elements from both array on left side is lower 
-        // than all elements from both arr at right side (don't forget input = sorted arrays)
 
         if(firstArrLeftItem <= secondArrRightItem && secondArrLeftItem <= firstArrRightItem) {
             if (totalLength % 2 === 0) {
